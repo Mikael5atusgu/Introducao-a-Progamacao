e9d0151e5d5ba6384f28f0414fcaffeec872bdc5
Mikael5Atusgu <gomesmiel6@gmail.com>
2025-08-14 10:38:55 -0300

    Add files via upload

---
+++ tarefa bimestre.py
@@ -0,0 +1,6 @@
+n1b = float(input("Digite a nota do 1º bimestre"))
+n2b = float(input("Digite a nota do 2º bimestre"))
+
+Nnec = float((60-(n1b*2 + n2b*2))/2)/3
+
+print("você precisa de",Nnec,"na nota dos 2 ultimos bimestres para passar")
