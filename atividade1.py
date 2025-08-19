a3f4da11bddad674f9400ed6a5d8e36898761f17
Mikael5Atusgu <gomesmiel6@gmail.com>
2025-08-19 08:49:26 -0300

    Código "-for <var> in range(<num>)"

---
+++ atividade1.py
@@ -0,0 +1,3 @@
+n = int(input("Digite o numero multiplicado:"))
+for nm in range(10) :
+    print(n,"X",nm+1,"=",((1+nm)*n))
---
+++ atividade2.py
@@ -0,0 +1,7 @@
+n = int(1)
+
+for nm in range(10) :
+    
+    if (n+(nm+1))%2 == 0 :
+        print(n+(nm+1))
+    
