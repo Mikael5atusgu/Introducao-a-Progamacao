b7969a4a1da9c48e76cb0f3bc6768b24cb905b36
Mikael5Atusgu <gomesmiel6@gmail.com>
2025-08-28 10:36:13 -0300

    Add files via upload

---
+++ Atividade Listra3 7.py
@@ -0,0 +1,12 @@
+ha = 1.10
+th = 1.50
+A = 100
+contagem = 0
+for anos in range(A) :
+    ha += 0.04
+    th += 0.02
+    contagem += 1
+
+    if ha>th :
+        print("O Homem-Aranha será mais alto do que o Thanos depois de:",contagem,"anos")
+        break
