2c234761d67e554c3403fb42ae7276d87df46d43
Mikael5Atusgu <gomesmiel6@gmail.com>
2025-09-02 08:03:25 -0300

    Delete Atividade Listra3 4.py

--- Atividade Listra3 4.py
+++
@@ -1,7 +0,0 @@
-altura = float(0)  
-Qjogadores = int(input("digite a quantidaoded de jogadores do time!"))
-for I in range(Qjogadores) :
-
-    altura = altura + float(input("Digite a altura dos jogadores em metros:"))
-
-print("a média de altura dos jogadores é:",altura/Qjogadores,"m")
