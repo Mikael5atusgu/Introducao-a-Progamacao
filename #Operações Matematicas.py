9cc930c473f65ba905ef426a9d36c1db3eccc391
Mikael5Atusgu <gomesmiel6@gmail.com>
2025-09-18 10:34:25 -0300

    Add files via upload

--- #Operações Matematicas.py
+++ #Operações Matematicas.py
@@ -2,20 +2,82 @@
 
 #Menu Inicial:
 
+import os
+print("----C E N T R O  D E  C O N T A S  M A T E M Á T I C A S---- \nDigite o numero correspondente da conta matemática! \n 1-Soma \n 2-Subtração \n 3-Multiplicação \n 4-Divisão \n 5-Par ou Impar \n 6-Fatorial \n 7-Primo \n --Digite ''sair'' para sair \n--Digite ''Voltar'' para voltar ao menu")
 
-menu = ("0")
-while menu == ("Sair"):
+opcao = input("Digite o opção que queira:")
+opcaoM = opcao.upper()
+
+while opcaoM != "SAIR":
+    
+    #Soma
+    if opcao == "1" :
+        valorS1 = int(input("Digite o primeiro valor:"))
+        valorS2 = int(input("Digite o segundo valor:"))
+        print("O valor de",valorS1,"+",valorS2," é ",valorS1+valorS2)
+    
+    #Subt
+    if opcao == "2" :
+        valorS1 = int(input("Digite o primeiro valor:"))
+        valorS2 = int(input("Digite o segundo valor:"))
+        print("O valor de",valorS1,"-",valorS2," é ",valorS1-valorS2)
+    
+    #Mult
+    if opcao == "3" :
+        valorS1 = int(input("Digite o primeiro valor:"))
+        valorS2 = int(input("Digite o segundo valor:"))
+        print("O valor de",valorS1,"x",valorS2," é ",valorS1*valorS2)
+    
+    #Divi
+    if opcao == "4" :
+        valorS1 = int(input("Digite o primeiro valor:"))
+        valorS2 = int(input("Digite o segundo valor:"))
+        print("O valor de",valorS1,"/",valorS2," é ",valorS1/valorS2)
+    
+    #Par e impar
+    if opcao == "5" :
+        valorS1 = int(input("Digite o valor:"))
+        
+        if valorS1%2 == 0:
+            print("O valor é PAR")
+        
+        else:
+            print("O valor é IMPAR")        
+    
+    #Fato
+    if opcao == "6" :
+        numero = int(input("Digite o numero que queira fatorar!: "))
+        fat=1
+        contagem = 0
+        while contagem != numero :
+            
+            contagem+=1
+            fat *= contagem
+        print("o seu numero fatorado é: ",fat)
+    
+    #Primo
+    if opcao == "7" :
+        numero = int(input("Digite um número: "))
+    if numero <= 1:
+        print("O número ",numero," não é primo.")
+    else:
+        divisor = 2
+        while divisor <= numero // 2:
+            if numero % divisor == 0:
+                print("O número ",numero," não é primo.")
+                break
+            divisor += 1
+        else:
+            print("O número ",numero," é primo.")
+
     
     
-    if menu == ("Voltar"):
-        print("----C E N T R O  D E  C O N T A S  M A T E M Á T I C A S---- \nDigite o numero correspondente da conta matemática! \n 1-Soma \n 2-Subtração \n 3-Multiplicação \n 4-Divisão \n 5-Par ou Impar \n 6-Fatorial \n 7-Primo \n --Digite ''sair'' para sair \n--Digite ''Voltar'' para voltar ao menu")
     
-    menu = input()
     
-    if menu == ("1"):
-        soma1= int(input("Digite o 1º numero a ser somado:"))
-        soma2= int(input("Digite o 2º numero a ser somado:"))
-        print("A Soma dos numeros são ",soma1+soma2)
-        menu = "Sair"
     
-menu = input("Voltar")
+    
+    input("Aperte ENTER para voltar ao menu")
+    os.system("cls")
+    print("----C E N T R O  D E  C O N T A S  M A T E M Á T I C A S---- \nDigite o numero correspondente da conta matemática! \n 1-Soma \n 2-Subtração \n 3-Multiplicação \n 4-Divisão \n 5-Par ou Impar \n 6-Fatorial \n 7-Primo \n --Digite ''sair'' para sair \n--Digite ''Voltar'' para voltar ao menu")
+    opcao = input("Digite o opção que queira:")
+    opcaoM = opcao.upper()
